477f54800fc5f03462ea37572a31698eefa16589
KristofferGus <109553487+mrSWE3@users.noreply.github.com>
2024-08-06 23:42:18 +0200

    created requierd structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-beat-detector-pipable',  # Replace with your package's name
+    version='0.0.0',  # Replace with your package's version
+    packages=find_packages(),
+    include_package_data=True,
+    #package_data={'python-beat-detector-pipable': ['py.typed']},  # Replace 'your_package' with your actual package name
+)
